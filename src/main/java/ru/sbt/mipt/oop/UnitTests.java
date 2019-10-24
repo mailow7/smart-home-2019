1fad161fa71b0988d60bc8cbc5b380c3e01d22fa
mailow7 <mailow@mail.ru>
2019-10-25 00:07:35 +0300

    set One Unit Test

--- src/main/java/ru/sbt/mipt/oop/UnitTests.java
+++ src/main/java/ru/sbt/mipt/oop/UnitTests.java
@@ -1,23 +1,35 @@
 package ru.sbt.mipt.oop;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitTests {
 
     @Test
-    public void DoorEventProcessorReturnTrue() {
+    public void CheckEventProcessorLightOnWhenDoorOpen() {
         DoorEventProcessor dooreventprocessor = new DoorEventProcessor();
-        assertEquals(true, dooreventprocessor.Work());
-        assertNotEquals(false, dooreventprocessor.Work());
-        assertTrue(dooreventprocessor.Work());
-    }
 
-    @Test
-    public void DoorEventProcessorReturnFasle() {
-        DoorEventProcessor dooreventprocessor = new DoorEventProcessor();
-        assertEquals(false, dooreventprocessor.ReturnFalse());
-        assertNotEquals(true, dooreventprocessor.ReturnFalse());
-        assertFalse(dooreventprocessor.ReturnFalse());
+        SmartHome smartHome = new SmartHome();
+        smartHome.GetHomeState();
+        Room room = smartHome.getRoom("hall");
+
+        Boolean LightisOn = true;
+
+        for (Light light : room.getLights()) {
+            light.setOn(false);
+        }
+
+        SensorEvent event1 = new SensorEvent(SensorEventType.DOOR_OPEN,"4");
+        dooreventprocessor.handle(smartHome,event1);
+
+        for (Light light : room.getLights()) {
+            if(!light.isOn()){
+               LightisOn=false;
+            }
+        }
+
+        assertEquals(true,LightisOn);
+
     }
 
 }
